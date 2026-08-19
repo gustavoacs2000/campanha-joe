221d03eda03127d03f382eee36ae3d56dab36690
Gustavo Alexandre <45288773+gustavoacs2000@users.noreply.github.com>
2026-08-19 15:42:47 -0300

    fix: carrega ajuste de layout mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import './mobile-fix.css';
 
 export const metadata: Metadata = {
   title: 'Volta Joe | Participe desse novo começo',
